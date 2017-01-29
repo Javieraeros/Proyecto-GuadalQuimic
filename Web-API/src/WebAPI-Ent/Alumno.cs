4b8a873832ee82ee0b8d7905ea83e04db384719c
Javier Ruiz WinCasa <Pajarrurro@gmail.com>
2017-01-29 19:48:59 +0100

    Iniciado el proyecto de la web api con las 4 capas.

---
+++ Web-API/src/WebAPI-Ent/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Ent
+{
+    public class Alumno
+    {
+        public Alumno()
+        {
+        }
+    }
+}
